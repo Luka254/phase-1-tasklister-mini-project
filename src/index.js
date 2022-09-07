bf3040ffdf32df44ac95b807bb3a09c6fd87d042
Luke <luke.kazungu@student.moringaschool.com>
2022-09-08 02:27:54 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const form = document.querySelector('form');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    populateToDoList(e.target.new_task_description.value);
+
+    form.reset();
+  });
+
+function populateToDoList(todoValue){
+
+  const li = document.createElement("li");
+  li.textContent = `${todoValue} `;
+  document.querySelector('#tasks').appendChild(li); 
+
+
+  const btn = document.createElement("button");
+  btn.textContent = "X";
+  li.appendChild(btn);
+  btn.addEventListener('click', deleteTasks);
+
+}
+
+function deleteTasks(e){
+  e.target.parentNode.remove();
+}
